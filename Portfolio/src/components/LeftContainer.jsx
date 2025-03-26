8a476ed6307f4e049d94735315d4cd9a1dea178f
httparch <shanebigornia123@gmail.com>
2025-03-26 23:16:15 +0800

    modify

--- Portfolio/src/components/LeftContainer.jsx
+++ Portfolio/src/components/LeftContainer.jsx
@@ -8,43 +8,28 @@ function LeftContainer() {
   const [activeTab, setActiveTab] = useState("projects");
 
   return (
-    <div className="flex flex-col space-y-5 w-full">
+    <div className="flex flex-col space-y-5 w-full h-full">
+      {/* Tabs */}
       <div className="flex bg-gray-500 rounded-md p-1 border border-gray-300 shadow-inner shadow-gray-700">
         <div className="flex space-x-1">
-          <button
-            className={`px-5 py-1 text-sm font-medium rounded-md transition-all duration-200 ${
-              activeTab === "projects"
-                ? "bg-white"
-                : "bg-gray-500 text-white cursor-pointer"
-            }`}
-            onClick={() => setActiveTab("projects")}
-          >
-            Projects
-          </button>
-          <button
-            className={`px-5 py-1 text-sm font-medium rounded-md transition-all duration-200  ${
-              activeTab === "certificates"
-                ? "bg-white"
-                : "bg-gray-500 text-white cursor-pointer"
-            }`}
-            onClick={() => setActiveTab("certificates")}
-          >
-            Certificates
-          </button>
-          <button
-            className={`px-5 py-1 text-sm font-medium rounded-md transition-all duration-200  ${
-              activeTab === "artworks"
-                ? "bg-white"
-                : "bg-gray-500 text-white cursor-pointer"
-            }`}
-            onClick={() => setActiveTab("artworks")}
-          >
-            Artworks
-          </button>
+          {["projects", "certificates", "artworks"].map((tab) => (
+            <button
+              key={tab}
+              className={`px-5 py-1 text-sm font-medium rounded-md transition-all duration-200 ${
+                activeTab === tab
+                  ? "bg-white"
+                  : "bg-gray-500 text-white cursor-pointer"
+              }`}
+              onClick={() => setActiveTab(tab)}
+            >
+              {tab.charAt(0).toUpperCase() + tab.slice(1)}
+            </button>
+          ))}
         </div>
       </div>
 
-      <div className="bg-gray-400 rounded-lg p-4 h-[650px] overflow-hidden shadow-inner shadow-gray-600">
+      {/* Content Panel */}
+      <div className="bg-gray-400 rounded-lg p-4 flex-1 overflow-hidden shadow-inner shadow-gray-600">
         {activeTab === "projects" ? (
           <div className="space-y-4 h-full overflow-y-auto custom-scrollbar p-3 pb-3">
             <Projects projects={projects} />
